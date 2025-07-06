beeefa8b1e9b96f10477eb75c4366e6e10667ec7
singo-i <i.singo@gmail.com>
2025-07-06 22:47:42 +0900

    テーマの初期設定とスタイルを追加し、404ページやアーカイブ、ホームページのテンプレートを実装

---
+++ wp-content/themes/miyoshi-kasei-kougyo/functions.php
@@ -0,0 +1,78 @@
+<?php
+
+/**
+ * miyoshi_kasei_kougyo functions and definitions.
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package WordPress
+ * @since miyoshi_kasei_kougyo 1.0
+ */
+
+// if (! function_exists('miyoshi_kasei_kougyo_editor_style')) :
+// 	/**
+// 	 * Enqueues editor-style.css in the editors.
+// 	 *
+// 	 * @since miyoshi_kasei_kougyo 1.0
+// 	 *
+// 	 * @return void
+// 	 */
+// 	function miyoshi_kasei_kougyo_editor_style()
+// 	{
+// 		add_editor_style(get_parent_theme_file_uri('assets/css/editor-style.css'));
+// 	}
+// endif;
+// add_action('after_setup_theme', 'miyoshi_kasei_kougyo_editor_style');
+
+if (! function_exists('miyoshi_kasei_kougyo_enqueue_styles')) :
+	/**
+	 * Enqueues style.css on the front.
+	 *
+	 * @since miyoshi_kasei_kougyo 1.0
+	 *
+	 * @return void
+	 */
+	function miyoshi_kasei_kougyo_enqueue_styles()
+	{
+		wp_enqueue_style(
+			'miyoshi_kasei_kougyo-style',
+			get_parent_theme_file_uri('style.css'),
+			array(),
+			wp_get_theme()->get('Version')
+		);
+	}
+endif;
+add_action('wp_enqueue_scripts', 'miyoshi_kasei_kougyo_enqueue_styles');
+
+
+if (! function_exists('miyoshi_kasei_kougyo_enqueue_block_styles')) :
+	/**
+	 * Enqueues block styles for the blocks that need them.
+	 *
+	 * @since miyoshi_kasei_kougyo 1.0
+	 *
+	 * @return void
+	 */
+	function miyoshi_kasei_kougyo_enqueue_block_styles()
+	{
+		// Add the block name (with namespace) for each style.
+		$blocks = array(
+			'core/navigation',
+		);
+
+		// Loop through each block and enqueue its styles.
+		foreach ($blocks as $block) {
+
+			// Replace slash with hyphen for filename.
+			$slug = str_replace('/', '-', $block);
+
+			wp_enqueue_block_style($block, array(
+				'handle' => "miyoshi_kasei_kougyo-block-{$slug}",
+				'src'    => get_theme_file_uri("assets/blocks/{$slug}.css"),
+				'path'   => get_theme_file_path("assets/blocks/{$slug}.css")
+			));
+		}
+	}
+endif;
+
+add_action('init', 'miyoshi_kasei_kougyo_enqueue_block_styles');
---
+++ wp-content/themes/miyoshi-kasei-kougyo/patterns/hidden-blog-heading.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Title: Hidden blog heading
+ * Slug: miyoshi-kasei-kougyo/hidden-blog-heading
+ * Description: Hidden heading for the home page and index template.
+ * Inserter: no
+ *
+ * @package WordPress
+ * @subpackage Miyoshi_Kasei_Kougyo
+ * @since Miyoshi Kasei Kougyo 1.0
+ */
+
+?>
+<!-- wp:heading {"textAlign":"left","level":1} -->
+<h1 class="wp-block-heading has-text-align-left"><?php esc_html_e('Blog', 'miyoshi-kasei-kougyo'); ?></h1>
+<!-- /wp:heading -->
