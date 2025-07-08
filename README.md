246cfb30471f294998f84fbb3d6ae6badd64628c
singo-i <i.singo@gmail.com>
2025-07-08 11:32:52 +0900

    .gitignoreのプラグインセクションを整理し、README.mdに新しい行を追加。ポストナビゲーションパターンを新規作成。

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # site-miyoshi-kasei-kougyo
+
 ミヨシ化成工業所のサイト
+
+xK0W%ghfWjG9To6EO1
---
+++ wp-content/themes/miyoshi-kasei-kougyo/patterns/post-navigation.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Title: Post navigation
+ * Slug: miyoshi-kasei-kougyo/post-navigation
+ * Categories: query
+ */
+?>
+<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group">
+  <!-- wp:post-navigation-link {"type":"previous","label":"<?php esc_html_e('Previous post', 'miyoshi-kasei-kougyo'); ?>"} /-->
+  <!-- wp:post-navigation-link {"label":"<?php esc_html_e('Next post', 'miyoshi-kasei-kougyo'); ?>"} /-->
+</div>
+<!-- /wp:group -->
